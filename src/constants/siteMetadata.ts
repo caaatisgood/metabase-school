c0cb342f1ab1c3a839507ad71225fefaf5a79a04
robin <almigod0718@gmail.com>
2021-04-17 02:04:57 +0800

    Add OG image and favicon

--- src/constants/siteMetadata.ts
+++ src/constants/siteMetadata.ts
@@ -1,6 +1,9 @@
 const siteMetadata = {
   title: 'Metabase School',
+  description: 'A Metabase-integrated, real-time collaborative tool for writing SQL',
   githubUrl: 'https://github.com/caaatisgood/metabase-school',
+  host: 'http://metabase-school.netlify.app',
+  creator: '@caaatisgood',
 }
 
 export default siteMetadata
